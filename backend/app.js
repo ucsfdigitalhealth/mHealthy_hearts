03dde8a8908c9f055e5df1af9e0b9cc9a48f1eb5
dubnation <dubnation@users.noreply.github.com>
2025-10-12 13:17:36 -0700

    added refresh token auth

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const authRoutes = require('./auth');
+const cookieParser = require('cookie-parser');
+const { router: authRoutes } = require('./auth');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST'],       
+    credentials: true                
+  }));
+
 app.use('/api/auth', authRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log('Server is running on port 3000');
